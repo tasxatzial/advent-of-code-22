f454f5107c7bfd7be0c267eda8e04570cdb4222e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-01 18:45:12 +0200

    day1: parse input

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -1,7 +1,27 @@
 (ns day01.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn str->int
+  [s]
+  (Integer/parseInt s))
+
+(defn parse-input
+  "Parses the input string and returns a sequence of collections.
+  Each collection represents the calories of each elf."
+  [s]
+  (->> s
+       clojure.string/split-lines
+       (partition-by #(= "" %))
+       (filter #(not= '("") %))
+       (map #(map str->int %))))
+
+(def calories (parse-input (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println calories))
