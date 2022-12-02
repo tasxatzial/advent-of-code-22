8f9fa342ed7809fa160e16430d9a60154a4710eb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-02 13:51:13 +0200

    day2: refactoring

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -29,7 +29,7 @@
    :loss 2})
 
 (def num->outcome
-  "Maps a number to an outcome. Reverses the mapping of outcome-num."
+  "Maps a number to an outcome. Reverses the mapping of outcome->num."
   (reverse-map outcome->num))
 
 ; --------------------------
@@ -37,22 +37,22 @@
 
 (def input-file "resources\\input.txt")
 
-(defn input-file->coll
+(defn input-file->strategy
   "Reads and parses the input file into a collection of vectors.
-  Each vector represents the -unencrypted- instructions in a round."
+  Each vector represents the -encrypted- instructions in a round."
   []
   (->> input-file
        slurp
        clojure.string/split-lines
        (map #(clojure.string/split % #" "))))
 
-(def memoized-data (memoize input-file->coll))
+(def memoized-strategy (memoize input-file->strategy))
 
-(defn decrypt-strategy-guide
+(defn decrypt-strategy
   "Decrypts the strategy guide using a decrypt function on each round."
   [decrypt-round-fn]
-  (let [data (memoized-data)]
-    (map decrypt-round-fn data)))
+  (let [strategy (memoized-strategy)]
+    (map decrypt-round-fn strategy)))
 
 (defn round-outcome
   "Returns :loss or :win or :draw depending on the outcome of the round."
@@ -60,28 +60,25 @@
   (let [[elf-choice my-choice] (map choice->num round)]
     (get num->outcome (mod (- my-choice elf-choice) 3))))
 
-(defn outcome-score
-  "Returns the score of the outcome of a round: 6 if won, 0 if lost, 3 if draw."
-  [round]
-  (case (round-outcome round)
-    :win 6
-    :loss 0
-    :draw 3))
+(def outcome->score
+  "Returns the score of an outcome."
+  {:win 6
+   :loss 0
+   :draw 3})
 
-(defn choice-score
-  "Returns the score of a choice: 3 for :scissors, 2 for :paper, 1 for :rock."
-  [s]
-  (case s
-    :scissors 3
-    :paper 2
-    :rock 1))
+(def choice->score
+  "Returns the score of a choice."
+  {:scissors 3
+   :paper 2
+   :rock 1})
 
 (defn round-score
   "Returns the total score of each round, that is, the sum of the
   shape-score and outcome-score."
   [round]
-  (let [my-selection (second round)]
-    (+ (outcome-score round) (choice-score my-selection))))
+  (let [my-choice (second round)
+        outcome (round-outcome round)]
+    (+ (outcome->score outcome) (choice->score my-choice))))
 
 (defn total-score
   "Calculates the total score when all rounds have ended."
@@ -93,43 +90,31 @@
 ; --------------------------
 ; problem 1
 
-(defn decrypt-symbol-p1
-  "Decrypt a symbol (p1).
-  Returns:
-  :rock for symbols A and X
-  :paper for symbols B and Y
-  :scissors for symbols C and Z"
-  [s]
-  (case s
-    ("A" "X") :rock
-    ("B" "Y") :paper
-    ("C" "Z") :scissors))
+(def decrypt-symbol-p1
+  "Decrypts a symbol (p1)."
+  {"A" :rock,
+   "X" :rock
+   "B" :paper,
+   "Y" :paper
+   "C" :scissors,
+   "Z" :scissors})
 
 (defn decrypt-round-p1
-  "Decrypts a round that consists of two symbols (p1)."
+  "Decrypts a round (p1)."
   [encrypted-round]
-  (map decrypt-symbol-p1 encrypted-round))
+  (mapv decrypt-symbol-p1 encrypted-round))
 
 ; --------------------------
 ; problem 2
 
-(defn decrypt-symbol-p2
-  "Decrypt a symbol (p2).
-  Returns:
-  :rock for symbol A
-  :paper for symbol B
-  :scissors for symbol C
-  :draw for symbol Y
-  :loss for symbol X
-  :win for symbol Z"
-  [s]
-  (case s
-    "A" :rock
-    "B" :paper
-    "C" :scissors
-    "Y" :draw
-    "X" :loss
-    "Z" :win))
+(def decrypt-symbol-p2
+  "Decrypts a symbol (p2)."
+  {"A" :rock
+   "B" :paper
+   "C" :scissors
+   "Y" :draw
+   "X" :loss
+   "Z" :win})
 
 (defn find-my-choice
   "Returns what I should play based on a given round (p2)."
@@ -140,9 +125,10 @@
     (get num->choice (mod (+ elf-choice-num outcome-num) 3))))
 
 (defn decrypt-round-p2
-  "Decrypts a round that consists of two symbols (p2)."
+  "Decrypts a round (p2). Returns a vector that contains the choices
+  of both players."
   [encrypted-round]
-  (let [decrypted-round (map decrypt-symbol-p2 encrypted-round)
+  (let [decrypted-round (map #(get decrypt-symbol-p2 %) encrypted-round)
         elf-choice (first decrypted-round)
         my-choice (find-my-choice decrypted-round)]
     [elf-choice my-choice]))
@@ -152,8 +138,8 @@
 
 (defn day02
   [decrypt-round-fn]
-  (let [strategy-guide (decrypt-strategy-guide decrypt-round-fn)]
-    (total-score strategy-guide)))
+  (let [decrypted-strategy (decrypt-strategy decrypt-round-fn)]
+    (total-score decrypted-strategy)))
 
 (defn day02-1
   []
