2f95a3dccae33e06548052844cbd0ffdaa0d578e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-02 09:06:07 +0200

    day2: parse input

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -1,7 +1,21 @@
 (ns day02.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-input
+  "Parses the input string and returns a sequence of vectors.
+  Each vector represents the instructions in a round."
+  [s]
+  (->> s
+    clojure.string/split-lines
+    (map #(clojure.string/split % #" "))))
+
+(def strategy (parse-input (slurp input-file)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println strategy))
