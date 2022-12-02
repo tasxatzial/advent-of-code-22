f4ae25444b4f4f82d7ad548118b200b1058a7d2d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-02 09:52:28 +0200

    day2: determine round outcome and scores

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -36,6 +36,37 @@
       parse-input
       decrypt-strategy))
 
+(defn round-outcome
+  "Returns :loss or :win or :draw depending on the outcome of the round."
+  [round]
+  (case round
+    ([:scissors :paper] [:paper :rock] [:rock :scissors]) :loss
+    ([:paper :scissors] [:rock :paper] [:scissors :rock]) :win
+    ([:paper :paper] [:scissors :scissors] [:rock :rock]) :draw))
+
+(defn outcome-score
+  "Returns the score of the outcome of a round: 6 if won, 0 if lost, 3 if draw."
+  [round]
+  (case (round-outcome round)
+    :win 6
+    :loss 0
+    :draw 3))
+
+(defn shape-score
+  "Returns the score of each shape: 3 for :scissors, 2 for :paper, 1 for :rock."
+  [s]
+  (case s
+    :scissors 3
+    :paper 2
+    :rock 1))
+
+(defn round-score
+  "Returns the total score of each round, that is, the sum of the
+  shape-score and outcome-score."
+  [round]
+  (let [my-selection (second round)]
+    (+ (outcome-score round) (shape-score my-selection))))
+
 (defn -main
   []
   (println decrypted-strategy))
