ee4d41683d67d77dba41c9daf0558a26d7d06bec
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-03 15:44:43 +0200

    day3: refactoring

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -15,21 +15,13 @@
 
 (def input-file "resources\\input.txt")
 
-(defn rucksack->compartments
-  "Converts a string that represents a rucksack to its compartment representation:
-  A collection of two equally sized lists, each containing the items in a compartment."
-  [rucksack]
-  (let [rucksack-size (count rucksack)]
-    (partition (/ rucksack-size 2) rucksack)))
-
 (defn input-file->rucksacks
-  "Reads and parses the input file into a collection rucksacks. Each rucksack
-  is split into two compartments."
+  "Reads and parses the input file into a collection rucksacks,
+  each rucksack is represented by a string."
   []
   (->> input-file
        slurp
-       clojure.string/split-lines
-       (map rucksack->compartments)))
+       clojure.string/split-lines))
 
 (def memoized-rucksacks (memoize input-file->rucksacks))
 
@@ -43,12 +35,19 @@
 ; --------------------------
 ; problem 1
 
-(defn get-rucksack-common-items
-  "Returns a collection of the items that appear in both compartments of each rucksack."
+(defn rucksack->compartments
+  "Converts a string that represents a rucksack to its compartment representation:
+  A collection of two equally sized lists, each containing the items in a compartment."
   [rucksack]
-  (let [first-compartment (first rucksack)
-        second-compartment (second rucksack)]
-    (seq (set/intersection (set first-compartment) (set second-compartment)))))
+  (let [rucksack-size (count rucksack)]
+    (partition (/ rucksack-size 2) rucksack)))
+
+(defn get-compartment-common-items
+  "Returns a set of the items that appear in both compartments of each rucksack."
+  [rucksack]
+  (let [compartments (rucksack->compartments rucksack)
+        compartments-set (map set compartments)]
+    (apply set/intersection compartments-set)))
 
 ; --------------------------
 ; results
@@ -56,7 +55,7 @@
 (defn day03-1
   []
   (->> (memoized-rucksacks)
-       (map get-rucksack-common-items)
+       (map (comp seq get-compartment-common-items))
        flatten
        (map get-item-priority)
        (apply +)))
