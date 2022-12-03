bc7003abbad5cdf3b8b79eebee0e50587fd90eab
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-03 13:54:38 +0200

    day3: parse input

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -1,7 +1,22 @@
 (ns day03.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-file->rucksacks
+  "Reads and parses the input file into a collection of sets.
+  Each set represents the contents in a rucksack."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       (map set)))
+
+(def memoized-rucksacks (memoize input-file->rucksacks))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (memoized-rucksacks)))
