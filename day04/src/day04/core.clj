9255dd0c9a3d6a49374a38bc380384b6a1f2e2be
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-04 12:51:38 +0200

    day4: parse input

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,7 +1,34 @@
 (ns day04.core
-  (:gen-class))
+    (:gen-class))
+
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-assignment->assignment
+      "Parses an assignment from the input file into an assignment that has the form (A B)"
+      [assignment]
+      (->> (clojure.string/split assignment #"-")
+           (map #(Integer/parseInt %))))
+
+(defn input-line->assignment-pair
+      "Parses an input line into an assignment pair that has the form ((A B) (C D))"
+      [line]
+      (->> (clojure.string/split line #",")
+           (map input-assignment->assignment)))
+
+(defn input-file->assignment-pairs
+      "Reads and parses the input file into a collection of assignment pairs.
+      Each pair has the form ((A B) (C D))"
+      []
+      (->> input-file
+           slurp
+           clojure.string/split-lines
+           (map input-line->assignment-pair)))
+
+(def memoized-assignment-pairs (memoize input-file->assignment-pairs))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+      []
+      (println (memoized-assignment-pairs)))
