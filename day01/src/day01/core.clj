84c532704b43b6bbffc576aa3413c5c55d0dbaad
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-06 00:56:25 +0200

    day1: refactoring

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -2,25 +2,41 @@
   (:gen-class))
 
 ; --------------------------
-; common
-
-(def input-file "resources\\input.txt")
+; utils
 
 (defn str->int
   [s]
   (Integer/parseInt s))
 
-(defn parse-input
-  "Parses the input string and returns a sequence of collections.
-  Each collection represents the calories of each elf."
-  [s]
-  (->> s
-       clojure.string/split-lines
-       (partition-by #(= "" %))
-       (filter #(not= '("") %))
-       (map #(map str->int %))))
+; --------------------------
+; common
 
-(def elves-calories (parse-input (slurp input-file)))
+(def input-file "resources\\input.txt")
+
+(defn partition-input-by-elf
+  "Partitions the input string by the calories of each elf."
+  [input-str]
+  (->> input-str
+       (partition-by #(= "" %))
+       (filter #(not= '("") %))))
+
+(defn input-calories->int-calories
+  "Accept a seq that contains calories represented by strings and returns a seq
+  that contains the calories represented by the corresponding integers."
+  [input-calories]
+  (map str->int input-calories))
+
+(defn input-file->elves-calories
+  "Reads and parses the input file into a seq of seqs.
+  Each seq contains integers that represent the calories of each elf."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       partition-input-by-elf
+       (map input-calories->int-calories)))
+
+(def memoized_input-file->elves-calories (memoize input-file->elves-calories))
 
 (defn elf-total-calories
   "Returns the total calories carried by an elf."
@@ -32,13 +48,13 @@
 
 (defn day01-1
   []
-  (->> elves-calories
+  (->> (memoized_input-file->elves-calories)
        (map elf-total-calories)
        (apply max)))
 
 (defn day01-2
   []
-  (->> elves-calories
+  (->> (memoized_input-file->elves-calories)
        (map elf-total-calories)
        (sort >)
        (take 3)
