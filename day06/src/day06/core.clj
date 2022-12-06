aada797f34153eb91bcce72e0073628368147b42
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-06 13:10:31 +0200

    day6: parse input

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -1,7 +1,19 @@
 (ns day06.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-file->signal
+  "Reads the input file and returns a string that represents the signal."
+  []
+  (-> input-file
+      slurp))
+
+(def memoized_signal (memoize input-file->signal))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (memoized_signal)))
