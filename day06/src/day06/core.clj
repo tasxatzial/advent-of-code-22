fc9a10965347d7f632220436a7fc6449b3e2faaa
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-06 21:14:23 +0200

    day6: find the index of a start marker

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -14,6 +14,26 @@
 
 (def memoized_signal (memoize input-file->signal))
 
+(defn start-marker?
+  "Returns true if the collection s is a start marker, false otherwise.
+  Being a start marker requires:
+  1) s has packet-length number of elements.
+  2) s has distinct elements."
+  [s packet-length]
+  (and (apply distinct? s) (= packet-length (count s))))
+
+(defn find-start-marker
+  "Finds the signal index (starting from 1) of the first start marker.
+  Returns -1 if a start marker is not found."
+  [signal packet-length]
+  (loop [_signal signal]
+    (if (first _signal)
+      (if (start-marker? (take packet-length _signal) packet-length)
+        (+ (count signal) (- (count _signal)) packet-length)
+        (recur (rest _signal)))
+      -1)))
+
+
 (defn -main
   []
   (println (memoized_signal)))
