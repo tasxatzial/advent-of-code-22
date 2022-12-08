70f44e17590c4d8b1477f8a1a4a2f2188a793c86
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-08 11:57:20 +0200

    day8: initial commit

---
+++ day08/README.md
@@ -0,0 +1,3 @@
+# day08
+
+[adventofcode.com/2022/day/8](https://adventofcode.com/2022/day/8)
---
+++ day08/src/day08/core.clj
@@ -0,0 +1,7 @@
+(ns day08.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
