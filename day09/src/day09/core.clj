518e43c38c48f74e1df9669e7ae1340be8ff2c30
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-11 17:37:52 +0200

    day9: add functions that return the new positions of knots as the move

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -33,6 +33,35 @@
 
 (def memoized_input-file->instructions (memoize input-file->instructions))
 
+(defn knot-distance->move-vector
+  "Given the distance vector (V2-V1) between two subsequent knots, it returns the
+  vector that needs to be added to V1 so that it touches V2.
+  The numbers in the distance vector should be integers with absolute value <= 2."
+  [[x y]]
+  (if (and (not= x -2) (not= x 2) (not= y -2) (not= y 2))
+    '(0 0)
+    (map #(get {-2 -1, 2 1} % %) [x y])))
+
+(defn move-head
+  "Moves the head knot in the given direction and returns its new position."
+  [head direction]
+  (let [[x y] head]
+    (case direction
+      :D (list x (dec y))
+      :U (list x (inc y))
+      :L (list (dec x) y)
+      :R (list (inc x) y))))
+
+(defn move-knot
+  "Moves the given knot towards the target knot and returns its new position.
+  Both knots must be subsequent."
+  [target-knot knot]
+  (let [[x-to-move y-to-move] knot
+        [x-moved y-moved] target-knot
+        diff [(- x-moved x-to-move) (- y-moved y-to-move)]
+        move-vector (knot-distance->move-vector diff)]
+    (map + knot move-vector)))
+
 (defn -main
   []
   (println (memoized_input-file->instructions)))
