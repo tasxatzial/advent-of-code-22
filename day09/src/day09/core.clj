3b650b058caa75852b7a5952a380059daf95720c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-11 18:42:30 +0200

    day9: execute a single instruction

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -8,6 +8,22 @@
   [s]
   (Integer/parseInt (str s)))
 
+(defn reduce-repeatedly
+  "Reduces the given collection given a 2-arity function and an initial value.
+  Traverses coll and for each element it evaluates f(R, el) using as R the
+  result from the previous step. The given val is used to initialize R.
+  It short-circuits if the result f(R, el) is the same as el."
+  [f val coll]
+  (loop [new-coll []
+         coll coll
+         init val]
+    (if-let [curr (first coll)]
+      (let [res (f init curr)]
+        (if (= res curr)
+          (into new-coll coll)
+          (recur (conj new-coll res) (rest coll) res)))
+      new-coll)))
+
 ; --------------------------
 ; common
 
@@ -61,6 +77,30 @@
         diff [(- x-moved x-to-move) (- y-moved y-to-move)]
         move-vector (knot-distance->move-vector diff)]
     (map + knot move-vector)))
+
+(defn execute-instruction
+  "Multi-arity function.
+  1) If called with 2 arguments, it receives an instruction and a seq of knots,
+  and executes the instruction returning a vector of two elements. First one is
+  a vector of the all the positions of the tail knot and the second one is a
+  vector of the final positions of all knots.
+  2) If called with 3 arguments, it can also receive a starting vector of tail
+  positions."
+  ([instruction knots]
+   (execute-instruction instruction knots []))
+  ([instruction knots tail-positions]
+   (let [[direction move-amount] instruction
+         head (first knots)
+         rest-knots (rest knots)]
+     (if (pos? move-amount)
+       (let [new-head (move-head head direction)
+             new-rest-knots (reduce-repeatedly move-knot new-head rest-knots)
+             new-knots (into [new-head] new-rest-knots)
+             new-tail (last new-knots)
+             new-tail-positions (conj tail-positions new-tail)
+             updated-instruction [direction (dec move-amount)]]
+         (recur updated-instruction new-knots new-tail-positions))
+       [tail-positions knots]))))
 
 (defn -main
   []
