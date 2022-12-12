c924337bedd81daf497d67d086b6e3e7031a6062
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-12 12:38:56 +0200

    day10: parse input

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -1,7 +1,39 @@
 (ns day10.core
   (:gen-class))
 
+; --------------------------
+; utils
+
+(defn str->int
+  [s]
+  (Integer/parseInt (str s)))
+
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-line->cmd
+  "Parses an input line into a command. Returns a vector, it's first element is a
+  keyword that describes the command, and the second element, if it exists,
+  is an integer."
+  [line]
+  (let [[op val] (clojure.string/split line #" ")]
+    (if val
+      [(keyword op) (str->int val)]
+      [(keyword op)])))
+
+(defn input-file->cmds
+  "Reads and parses the input file into a vector of commands. The form of each
+  command is described in the function input-line->cmd."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       (mapv input-line->cmd)))
+
+(def memoized_input-file->cmds (memoize input-file->cmds))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (memoized_input-file->cmds)))
