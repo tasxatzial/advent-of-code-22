4399548127ddd3499c91ac371b90dca78e6e52f4
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-12 12:49:13 +0200

    day10: generate the register values in every cycle

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -34,6 +34,34 @@
 
 (def memoized_input-file->cmds (memoize input-file->cmds))
 
+(def get-cycle-cost
+  {:addx 2
+   :noop 1})
+
+(defn exec-cmd
+  "Accepts a vector the represents a command and the current value of the
+  register. Returns a vector of all the register values that have been
+  generated when the command is completed."
+  [cmd register]
+  (let [[op val] cmd
+        cycle-cost (get-cycle-cost op)
+        first-registers (vec (take (dec cycle-cost) (repeat register)))]
+    (case op
+      :addx (conj first-registers (+ val register))
+      (conj first-registers register))))
+
+(defn get-cycles-register
+  "Accepts a vector of commands and an initial register. Returns a vector
+  that has the value of the register in each cycle when all commands
+  have been completed. Includes the initial value of the register."
+  [cmds register]
+  (reduce (fn [result cmd]
+            (let [last-register (last result)
+                  new-registers (exec-cmd cmd last-register)]
+              (into result new-registers)))
+          [register]
+          cmds))
+
 (defn -main
   []
   (println (memoized_input-file->cmds)))
