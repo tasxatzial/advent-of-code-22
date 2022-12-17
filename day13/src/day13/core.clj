5c137216940e5c539126b2c521922b6a1edbe969
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-17 15:28:12 +0200

    day13: parse input

--- day13/src/day13/core.clj
+++ day13/src/day13/core.clj
@@ -1,6 +1,28 @@
 (ns day13.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-line->packet
+  "Parses an input line into a vector."
+  [line]
+  (when (not= "" line)
+    (read-string line)))
+
+(defn input-file->packets
+  "Reads and parses the input file into a seq of packets (vectors)."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       (map input-line->packet)
+       (filter some?)))
+
+(def memoized_input-file->packets (memoize input-file->packets))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
