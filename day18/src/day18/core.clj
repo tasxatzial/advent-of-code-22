2edf29072e51047051231314cfce84b1d506c534
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-19 12:27:41 +0200

    day18: parse input
    day18: parse input

--- day18/src/day18/core.clj
+++ day18/src/day18/core.clj
@@ -1,7 +1,30 @@
 (ns day18.core
   (:gen-class))
 
+
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-line->droplet-position
+  "Parses an input line into a position (vector of 3 ints)."
+  [line]
+  (map #(Integer/parseInt %) (clojure.string/split line #",")))
+
+(defn input-file->droplet-positions
+  "Reads and parses the input file into a seq of positions.
+  Each position is a vector of 3 ints."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       (map input-line->droplet-position)
+       set))
+
+(def memoized_input-file->droplet-positions
+  (memoize input-file->droplet-positions))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (memoized_input-file->droplet-positions)))
