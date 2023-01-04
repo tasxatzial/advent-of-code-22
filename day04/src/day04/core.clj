5d65ba9b1d8338847305c196a2de8f483f0b1a33
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-04 16:57:42 +0200

    day4: update docstrings

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -7,26 +7,26 @@
 (def input-file "resources\\input.txt")
 
 (defn input-assignment->assignment
-      "Parses an assignment that is represented by a string into an assignment that has
-      the form (A B)."
-      [assignment]
-      (->> (clojure.string/split assignment #"-")
-           (map #(Integer/parseInt %))))
+  "Parses an assignment that is represented by the string A-B into an assignment that has
+  the form (A B)."
+  [assignment]
+  (->> (clojure.string/split assignment #"-")
+       (map #(Integer/parseInt %))))
 
 (defn input-line->assignment-pair
-      "Parses an input line into an assignment pair that has the form ((A B) (C D))."
-      [line]
-      (->> (clojure.string/split line #",")
-           (map input-assignment->assignment)))
+  "Parses an input line into an assignment pair that has the form ((A B) (C D))."
+  [line]
+  (->> (clojure.string/split line #",")
+       (map input-assignment->assignment)))
 
 (defn input-file->assignment-pairs
-      "Reads and parses the input file into a collection of assignment pairs.
-      Each pair has the form ((A B) (C D))."
-      []
-      (->> input-file
-           slurp
-           clojure.string/split-lines
-           (map input-line->assignment-pair)))
+  "Reads and parses the input file into a seq of assignment pairs.
+  Each pair has the form ((A B) (C D))."
+  []
+  (->> input-file
+       slurp
+       clojure.string/split-lines
+       (map input-line->assignment-pair)))
 
 (def memoized-assignment-pairs (memoize input-file->assignment-pairs))
 
@@ -56,9 +56,9 @@
 ; results
 
 (defn day04
-  [overlap_fn]
+  [fn_overlap]
   (->> (memoized-assignment-pairs)
-       (filter overlap_fn)
+       (filter fn_overlap)
        count))
 
 (defn day04-1
